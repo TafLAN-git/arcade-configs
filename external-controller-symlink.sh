244cd4fca057b3c5e687629a088d895234237a86
Rasmus Eskola <rasmus.eskola@gmail.com>
2013-12-12 21:11:42 +0200

    script for toggling external controller configs

---
+++ external-controller-symlink.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# make sure we're in the correct directory
+cd "$(dirname $0)"
+CWD=$(pwd)
+
+# 1 as argument = set external controller configs, 0 = vice versa
+to_external=$1
+
+# loop through all files in external-controller-configs/
+find "external-controller-configs/" -type f -print0 | while read -d $'\0' FILE; do
+	# remove external-controller-configs/ part of path
+	FILE=$(echo $FILE | sed 's/external-controller-configs\///')
+
+	#only operate on already existing symlinks
+	if [ -L "$HOME/$FILE" ]; then
+		rm "$HOME/$FILE"
+		if [[ $to_external == 0 ]]; then
+			echo "$FILE => $HOME/$FILE"
+			ln -sr "$FILE" "$HOME/$FILE"
+		else
+			echo "external-controller-configs/$FILE => $HOME/$FILE"
+			ln -sr "external-controller-configs/$FILE" "$HOME/$FILE"
+		fi
+	fi
+done

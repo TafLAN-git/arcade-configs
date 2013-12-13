3f3cfb3a0efcc42f497d1579fff6ad6120575456
sorlag <sorlag@taflan.tf>
2013-12-13 10:08:14 +0200

    separate config for sideways wiimote / wiimote + nunchuk

--- symlink.sh
+++ symlink.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # recursively symlink all files from paths in this array
-FILES=( bin/ .config .dolphin-gc .dolphin-gcwii .dolphin-wii .fceux .pcsx .wahcade .zsnes )
+FILES=( bin/ .config .dolphin-gc .dolphin-gcwii .dolphin-wiisideways .dolphin-wiinunchuk .fceux .pcsx .wahcade .zsnes )
 
 # symlink these directories
 DIRS=()

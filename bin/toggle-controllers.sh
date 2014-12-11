b395fab94a976745d76df3a92f3f5555976a0093
sorlag <sorlag@taflan.tf>
2014-12-11 20:04:20 +0200

    more comments

--- bin/toggle-controllers.sh
+++ bin/toggle-controllers.sh
@@ -3,12 +3,14 @@
 external_controllers=$(cat ~/controller_state)
 
 if [[ $external_controllers == 0 ]]; then
+    # enable external controllers
 	~/arcade-configs/external-controller-symlink.sh 1
 	echo 1 > ~/controller_state
 	killall -9 python dolphin-emu fceux zsnes pcsx vba vbam dgen mupen64plus mame # TODO: pids! groups?
 	sleep 1
 	wahcade &
 else
+    # disable external controllers
 	~/arcade-configs/external-controller-symlink.sh 0
 	echo 0 > ~/controller_state
 	killall -9 python dolphin-emu fceux zsnes pcsx vba vbam dgen mupen64plus mame # TODO: pids! groups?

bd5fbc9b2e7a02c6c0ef1a6190a4432ae8052ff2
sorlag <sorlag@taflan.tf>
2015-08-13 17:17:08 +0300

    big-ass commit

--- bin/toggle-controllers.sh
+++ bin/toggle-controllers.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-external_controllers=$(cat ~/controller_state)
+#external_controllers=$(cat ~/controller_state)
 
-if [[ $external_controllers == 0 ]]; then
+#if [[ $external_controllers == 0 ]]; then
+if [[ $1 == 'xbox360' ]]; then
     # enable external controllers
 	~/arcade-configs/external-controller-symlink.sh 1
 	echo 1 > ~/controller_state
-	killall -9 python2 dolphin-emu fceux mednafen pcsx vba vbam dgen mupen64plus mame # TODO: pids! groups?
-	sleep 1
-	wahcade &
+	killall -9 python2 retroarch dolphin-emu fceux mednafen pcsx gvbam dgen mupen64plus mame # TODO: pids! groups?
+	DISPLAY=:0 wahcade &
 else
-    # disable external controllers
+    # enable arcade controllers
 	~/arcade-configs/external-controller-symlink.sh 0
 	echo 0 > ~/controller_state
-	killall -9 python2 dolphin-emu fceux mednafen pcsx vba vbam dgen mupen64plus mame # TODO: pids! groups?
-	sleep 1
-	wahcade &
+	killall -9 python2 retroarch dolphin-emu fceux mednafen pcsx gvbam dgen mupen64plus mame # TODO: pids! groups?
+	DISPLAY=:0 wahcade &
 fi
--- bin/volume.sh
+++ bin/volume.sh
@@ -2,8 +2,8 @@
 
 if [[ $1 == "u" ]]; then
 	echo volume up
-    pactl set-sink-volume @DEFAULT_SINK@ -- +4%; paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+    pactl set-sink-volume @DEFAULT_SINK@ +4%; paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
 elif [[ $1 == "d" ]]; then
 	echo volume down
-    pactl set-sink-volume @DEFAULT_SINK@ -- -4%; paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+    pactl set-sink-volume @DEFAULT_SINK@ -4%; paplay /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
 fi

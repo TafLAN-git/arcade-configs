c60fbf23efc615191c8a0dc0dd9a5ed489ea138f
sorlag <sorlag@taflan.tf>
2016-01-16 19:15:34 +0200

    wahcade -> emulationstation refactor

--- bin/toggle-controllers.sh
+++ bin/toggle-controllers.sh
@@ -4,10 +4,10 @@ if [[ $1 == 'xbox360' ]]; then
     # enable external controllers
 	bash ~/arcade-configs/external-controller-symlink.sh 1
 	killall -9 python2 retroarch dolphin-emu
-	DISPLAY=:0 wahcade &
+	DISPLAY=:0 emulationstation &
 else
     # enable arcade controllers
 	bash ~/arcade-configs/external-controller-symlink.sh 0
 	killall -9 python2 retroarch dolphin-emu
-	DISPLAY=:0 wahcade &
+	DISPLAY=:0 emulationstation &
 fi
